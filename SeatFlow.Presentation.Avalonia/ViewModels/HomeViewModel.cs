da8b275cf48fcb89b137eb1f87b1e15d55ba5443
Helio-RC <o2567347413@outlook.com>
2026-07-18 01:18:56 +0800

    主页重构+修复窗口大小调整

--- SeatFlow.Presentation.Avalonia/ViewModels/HomeViewModel.cs
+++ SeatFlow.Presentation.Avalonia/ViewModels/HomeViewModel.cs
@@ -1,3 +1,4 @@
+using System;
 using SeatFlow.Presentation.Avalonia.Lang;
 
 namespace SeatFlow.Presentation.Avalonia.ViewModels;
@@ -6,4 +7,20 @@ public partial class HomeViewModel : ViewModelBase
 {
     public string Greeting { get; } = Resources.Home_Greeting;
     public string Subtitle { get; } = Resources.Home_Subtitle;
+
+    public string TimeBasedGreeting { get; }
+    public string VersionLabel { get; }
+
+    public HomeViewModel ()
+    {
+        var hour = DateTime.Now.Hour;
+        TimeBasedGreeting = hour switch
+        {
+            < 12 => Resources.Home_Greeting_Morning,
+            < 18 => Resources.Home_Greeting_Afternoon,
+            _    => Resources.Home_Greeting_Evening,
+        };
+
+        VersionLabel = string.Format(Resources.Home_Version, VersionInfo.Version);
+    }
 }
